7756af508f34f88372df1adcae994ea0407f6eeb
TWWWX <tulin5937@gmail.com>
2026-09-08 00:05:22 +0800

    首页新增竹子装饰背景（仅首页渲染，位于内容下层）

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,8 @@
   <div id="app-root">
     <!-- 主页 -->
     <div v-if="!mode" class="homepage">
+      <!-- 竹子装饰：仅首页渲染，位于背景之上、内容之下 -->
+      <img class="bamboo" src="/竹.png" alt="">
       <div class="home-header">
         <div class="home-title-row">
           <span class="title-line"></span>
@@ -995,8 +997,8 @@ export default {
         }
 
         // 表尾
-        const leftStr = '网站制作：蟋蟀  诗文筛汇：嫻菜无敌 蟋蟀';
-        const rightStr = '聚友：欢迎关注公众号「东坡墙」QQ「3301590656」';
+        const leftStr = '网站制作：蟋蟀丨诗文筛汇：嫻菜无敌&蟋蟀';
+        const rightStr = '欢迎关注公众号「东坡墙」QQ「3301590656」';
         const centerStr = 'www.sudongpo521.cn';
         const footerRow =
           `<tr style="height:auto;">
