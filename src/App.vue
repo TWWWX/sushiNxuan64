3373ca3ba905641298fb80ef1a414c602e235f32
TWWWX <tulin5937@gmail.com>
2026-09-08 12:44:03 +0800

    作者注内容包进单一span，修复a标签拆分flex项导致的版面错乱

--- src/App.vue
+++ src/App.vue
@@ -15,13 +15,13 @@
 
       <div class="author-note-wrapper">
         <div class="author-note-bar"></div>
-        <div class="author-note-box">游戏玩法：从库中那么多苏轼作品中，选出你最喜欢或认为最有代表性的64篇，制作成“我心目中的苏轼诗文TOP64”表格。<br>
+        <div class="author-note-box"><span>游戏玩法：从库中那么多苏轼作品中，选出你最喜欢或认为最有代表性的64篇，制作成“我心目中的苏轼诗文TOP64”表格。<br>
 制作完成后，点击“上传CSV”即可参与排行榜票数统计。<br>
 <br>
 技术支持：蟋蟀 丨 诗文筛汇：娴菜无敌 蟋蟀<br>
 欢迎关注公众号「东坡墙」QQ「<a href="https://qm.qq.com/q/sogmSgxpcW" target="_blank" rel="noopener">3301590656</a>」<br>
 <br>
-点开全文能看到的“编者备注”中，有部分译文、背景介绍等复制于网络，如有疏漏，还请见谅。</div>
+点开全文能看到的“编者备注”中，有部分译文、背景介绍等复制于网络，如有疏漏，还请见谅。</span></div>
       </div>
 
       <div class="entry-cards">
